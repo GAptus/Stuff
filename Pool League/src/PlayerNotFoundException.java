03be45e371014a79402965cec46c6577d9222793
Giacomo <giacomo@aptusinteractive.com>
2013-04-30 09:55:54 +0100

    preliminary PLayers class

---
+++ Pool League/src/PlayerNotFoundException.java
@@ -0,0 +1,4 @@
+
+public class PlayerNotFoundException extends Exception {
+
+}
---
+++ Pool League/src/PlayersTest.java
@@ -0,0 +1,10 @@
+import java.util.Collections;
+
+
+public class PlayersTest {
+	public static void main(String[] args) {
+		SortingThePlayers sorting = new SortingThePlayers();
+		
+		
+	}
+}
